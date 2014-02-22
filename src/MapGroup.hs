f5e2dc42ea9f20ba87ea7e7c734c8932d0ff51b9
Jesus Pardillo Vela <jesus.pardillo.vela@ericsson.com>
2014-02-22 10:29:05 +0100

    adding haddock

--- src/MapGroup.hs
+++ src/MapGroup.hs
@@ -5,20 +5,32 @@ import Data.Tuple (swap)
 import System.Process (readProcess)
 import Text.Regex.Posix ((=~))
 
-mapGroup :: String -> String -> [String] -> IO [String]
+-- | map a command to groups of consecutive lines matching a pattern
+mapGroup :: String   -- ^ filename
+         -> String   -- ^ pattern
+         -> [String] -- ^ lines
+         -> IO [String]
 mapGroup f g l = (sequence . map (either left right) . group g . lineno $ l) >>= return . concat
-   where left ::(Integer, String) -> IO [String]
-         left = return . return .snd
-         right :: [(Integer, String)] -> IO [String]
-         right x = readProcess (cmd f) (args f) (unlines . map snd $ x) >>= return . lines
-         cmd  = head . words
-         args = tail . words
+   where
+      left ::(Integer, String) -> IO [String]
+      left = return . return . snd
 
-group :: String -> [(Integer, String)] -> [Either (Integer,String) [(Integer, String)]]
+      right :: [(Integer, String)] -> IO [String]
+      right x = readProcess (cmd f) (args f) (unlines . map snd $ x) >>= return . lines
+      cmd  = head . words
+      args = tail . words
+
+-- | group
+group :: String              -- ^ pattern
+      -> [(Integer, String)] -- ^ lines including line numbers
+      -> [Either (Integer,String) [(Integer, String)]]
 group pattern = map (\ ls -> if length ls == 1
                              then Left  (head ls)
-                             else Right ls)
-              . groupBy (\ (_,l) (_,l') -> (l =~ pattern) && (l' =~ pattern))
+                             else Right ls
+                    )
+              . groupBy (\ (_, l) (_, l') -> (l =~ pattern) && (l' =~ pattern))
 
-lineno :: [String] -> [(Integer,String)]
+-- | append line numbers starting by one
+lineno :: [String] -- ^ lines
+       -> [(Integer,String)]
 lineno = map swap . flip zip [1..]
